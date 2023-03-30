19346826d3c29df8f863068c7a89c9e6ba599c34
carolineolagunju <olamidejr21@gmail.com>
2023-03-29 18:10:00 -0600

    Use function to say hello

---
+++ hello.js
@@ -0,0 +1,19 @@
+const sayHello = function () {
+  console.log("Hello, world")
+}
+sayHello();
+
+const hello  = function (name) {
+  console.log("Hello, " + name);
+}
+hello("Miranda");
+hello("Caroline");
+hello("Caliban");
+hello("Ferdinand");
+
+
+
+
+
+
+
